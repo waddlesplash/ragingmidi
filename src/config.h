df5affa8146081233d3d8bde732db5744ba973b2
waddlesplash <ajcsweb@gmail.com>
2012-12-22 14:34:57 -0500

    Add docs for src/config.h.

--- src/config.h
+++ src/config.h
@@ -1,6 +1,13 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+/****h* RagingMidi/config.h
+ * PURPOSE
+ *   Defines various global variables for
+ *   use by other components.
+ ******
+ */
+
 #define RM_VERSION "0.0.1"
 
 #endif // CONFIG_H
