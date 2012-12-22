41be8093fde68f2c8b5badd25082229f356fd330
waddlesplash <ajcsweb@gmail.com>
2012-12-22 15:29:28 -0500

    Remove needless OpenGL commented code.

--- src/Main.cpp
+++ src/Main.cpp
@@ -34,10 +34,7 @@
 #include "Gui/MainWind.h"
 
 int main(int argc, char *argv[])
-{ /* // experimental GL backend for all widgets
-#ifndef QT_NO_OPENGL
-    QApplication::setGraphicsSystem("opengl");
-#endif*/
+{
     QApplication a(argc, argv);
     MainWind w;
     w.show();
